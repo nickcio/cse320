1a70356f0315fda26be9731f5b1a70cc61e1a97e
Nicholas Ciotoli <nciotoli@cs.stonybrook.edu>
2023-03-14 20:33:10 -0400

    init free lists

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -2,12 +2,11 @@
 #include "sfmm.h"
 
 int main(int argc, char const *argv[]) {
+
     double* ptr = sf_malloc(sizeof(double)*4);
-
-    *ptr = 320320320e-320;
-
+    fprintf(stderr,"HERE\n");
     printf("%f\n", *ptr);
-
+    fprintf(stderr,"HERE\n");
     sf_free(ptr);
 
     return EXIT_SUCCESS;
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -12,15 +12,45 @@ static size_t totalsz = 0;
 static size_t allocsz = 0;
 static size_t MIN_BLOCK_SIZE = 32;
 
+void addfl(sf_block *block, int i) {
+    sf_block *nexter = sf_free_list_heads[i].body.links.next;
+    block->body.links.next = nexter;
+    block->body.links.prev = &sf_free_list_heads[i];
+    nexter->body.links.prev = block;
+    sf_free_list_heads[i].body.links.next = block;
+}
+
 void newmem() { //Memgrow + coalescing
     int init = 0;
     if(sf_mem_start() == sf_mem_end()) init = 1; //Whether this is the first page allocated, to know if we have to make prologue
     void *s = sf_mem_grow(); //Allocated page of memory
     if(init) { //Make prologue
-        sf_block prolog;
-        sf_header proh = (32 << 3)+1; //Header for 32 byte block, alloc flag == 1
-        prolog.header = proh;
+        sf_block prolog; //Prologue
+        prolog.header = (sf_header)(32 | THIS_BLOCK_ALLOCATED); //Header for 32 byte block, alloc flag == 1
+        *((sf_block*)s) = prolog;
+        sf_show_block(s);
+
+        sf_block *bigfree = s+32; //Pointer to first free block
+        bigfree->header = (sf_header)((PAGE_SZ-MIN_BLOCK_SIZE-8)); //Sets big sized free block, alloc flag = 0
+        sf_footer *bigfoot = s+32+(PAGE_SZ-MIN_BLOCK_SIZE-16);
+        *bigfoot = (sf_footer)(bigfree->header);
+
+        for(int i = 0; i < NUM_FREE_LISTS; i++) { //Init free lists
+            sf_block dummy;
+            dummy.body.links.next = &sf_free_list_heads[i];
+            dummy.body.links.prev = &sf_free_list_heads[i];
+            sf_free_list_heads[i] = dummy;
+        }
+
+        addfl(bigfree,9); //Add block to free list 9
     }
+    sf_block *end = (sf_mem_end()-sizeof(sf_header)); //Where epilogue will be
+    //fprintf(stderr,"end pointer: %p, epilog p: %p, diff: %ld\n",sf_mem_end(),end,((long int)sf_mem_end()-(long int)end));
+    sf_block epilog;
+    sf_header epih = 0 | THIS_BLOCK_ALLOCATED;
+    epilog.header = epih;
+    *end=epilog; //Sets epilogue
+    fprintf(stderr,"size of header: %ld, %ld\n",sizeof(sf_header), ((long int)s)%8);
 }
 
 void *addql(int index) { //Assumes index is valid
@@ -45,7 +75,7 @@ void *addql(int index) { //Assumes index is valid
 
 void *sf_malloc(size_t size) {
     if(size == 0) return NULL; //Size 0, return NULL
-    size_t fullsize = size%8 == 0 ? size + 16 : (size + 16)+(8-size%8); //Round to multiple of 8, fullsize is size with header
+    size_t fullsize = size%8 == 0 ? size + 8 : (size + 16)+(8-size%8); //Round to multiple of 8, fullsize is size with header
     if(sf_mem_start() == sf_mem_end()) { //If total mem pages is 0, mem grow
         newmem();
         totalsz+=PAGE_SZ;
@@ -56,23 +86,23 @@ void *sf_malloc(size_t size) {
     //sf_block newb; //New block
     //newb.header = nh; //Set header
     //Check if it is valid for quick lists
-    void *g = 0;
+    void *g = (void *)0;
     if(fullsize >= MIN_BLOCK_SIZE && fullsize <= MIN_BLOCK_SIZE+(NUM_QUICK_LISTS-1)*8) {
         int qind = (fullsize - MIN_BLOCK_SIZE)/8;
         fprintf(stderr,"qind: %d\n",qind);
-        g = addql(qind);
+        //g = addql(qind);
         fprintf(stderr,"after\n");
     }
     fprintf(stderr,"memstart: %p\n",start);
     fprintf(stderr,"size: %ld newsize: %ld\n",size,fullsize);
     fprintf(stderr,"durr: %p, %ld\n",start,totalsz);
-    //sf_show_block(&newb);
+
+    sf_show_heap();
     return g;
 }
 
 void sf_free(void *pp) {
-    // TO BE IMPLEMENTED
-    abort();
+    fprintf(stderr,"SF_FREE %p\n",pp);
 }
 
 void *sf_realloc(void *pp, size_t rsize) {
